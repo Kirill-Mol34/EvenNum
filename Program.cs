45d32fc3eb8447e974ce9144e7070e76795849b3
Kirill <kir.molokoff@yandex.ru>
2023-01-18 23:29:27 +0900

    fin

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.Write("Введите число:    ");
+string writeNum = Console.ReadLine();
+int num = Convert.ToInt32(writeNum);
+string result = $"{num} является чётным";
+
+if (num % 2 != 0) result = result.Replace("является", "не является");
+
+System.Console.WriteLine(result);
